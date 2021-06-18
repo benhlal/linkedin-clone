eed943ed4c11f7c478cb3d5739f6c2c34c95c0d9
benhlal <benhlalyounes@gmail.com>
2021-06-19 01:09:56 +0200

    [LinkedIn-Clone-v0] [feature] firebase new config

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,15 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 import firebase from 'firebase'
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCbY1mwvQPqYYxkJfqJl9MFDhSgzGjfLrc",
-    authDomain: "linkedin-9c08a.firebaseapp.com",
-    projectId: "linkedin-9c08a",
-    storageBucket: "linkedin-9c08a.appspot.com",
-    messagingSenderId: "343239021678",
-    appId: "1:343239021678:web:e681a152fd7d99c286827f",
-    measurementId: "G-T8Y65R2LN1"
+    apiKey: "AIzaSyCZkCGyivVGknnzFygt2n71r4yMS3cEc_s",
+    authDomain: "linkedin-ee94e.firebaseapp.com",
+    projectId: "linkedin-ee94e",
+    storageBucket: "linkedin-ee94e.appspot.com",
+    messagingSenderId: "22701780742",
+    appId: "1:22701780742:web:a8fbbe1b7dc6e1b385b887",
+    measurementId: "G-B488HFXZJJ"
 };
-
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
